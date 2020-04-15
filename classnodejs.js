e909d9a93342b9e39ee2e26e36d4c14ad37fc077
Ashik Md Shakil <ashikmdshakilpranto@gmail.com>
2020-04-16 00:29:08 +0600

    made object by class

---
+++ classnodejs.js
@@ -0,0 +1,12 @@
+class Student{
+    constructor(id, name, department){
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }   
+}
+var pranto = new Student();
+pranto.id = "171-35-1882";
+pranto.name = "ashik md shakil pranto";
+pranto.department = "SWE";
+console.log(pranto);
