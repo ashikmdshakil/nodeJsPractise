aae7e2888a99f399b71bd90cfe6cb0da6874af8c
Ashik Md Shakil <ashikmdshakilpranto@gmail.com>
2020-04-16 00:10:06 +0600

    this is object practise by function

---
+++ objectnodejs.js
@@ -0,0 +1,7 @@
+function Student(id , name , department){
+    this.id = id;
+    this.name = name;
+    this.department = department;
+}
+ var student = new Student("171-35-1882","Ashik Muhammad Shakil","SWE");
+console.log(student);
