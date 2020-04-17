8ec5bfd205740bf63397779436d50aa45054e8e8
Ashik Md Shakil <ashikmdshakilpranto@gmail.com>
2020-04-17 18:24:09 +0600

    practised getiing parameters from url

--- express.js
+++ express.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express(); 
 
 app.get('/', (req , res) =>{
-    res.send('Hello World');
+    res.send('Hello World!');
 });
-app.listen(3006);
+app.get('/test/:name', (request, response) =>{
+     response.send(request.params);
+});
+const port = process.env.PORT || 3000
+app.listen(3000 , () => console.log('server is running .......'));
